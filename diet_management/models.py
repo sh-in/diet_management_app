5ec58a7cf014b3950ab1d40e235a1f898d6b1323
Shin Suzuki <shin.shin.desktoppc@gmail.com>
2022-03-14 14:08:09 +0900

    update meal model

--- diet_management/models.py
+++ diet_management/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Meal(models.Model):
+    title = models.CharField(max_length=50)
+    calory = models.FloatField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+    carb = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
