360ecdf0632d5067a496c89e00985fa53af13a3b
Shin Suzuki <shin.shin.desktoppc@gmail.com>
2022-03-18 10:11:33 +0900

    update introduce django-bootstrap5 and update templates

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'diet_management.apps.DietManagementConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
