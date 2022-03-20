8dfc5a9b8b9f1345e55c0cc550eb36f05719a8a9
Shin Suzuki <shin.shin.desktoppc@gmail.com>
2022-03-20 12:27:28 +0900

    update form appearence

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'diet_management.apps.DietManagementConfig',
     'django_bootstrap5',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- diet_management/forms.py
+++ diet_management/forms.py
@@ -1,19 +1,26 @@
-from dataclasses import fields
 from django import forms
 from .models import Meal
 
 class MealPost(forms.ModelForm):
     class Meta():
         model = Meal
-        fields = "__all__"
+        fields = '__all__'
+        labels = {
+            'title': 'Meal name',
+            'calory': 'Calory',
+            'protein': 'Protein(g)',
+            'fat': 'Fat(g)',
+            'carb': 'Carbs(g)',
+            'date': 'Date',
+        }
+
         widgets = {
             "date": forms.SelectDateWidget
         }
-        lables = {
-            "title": "Meal name",
-            "calory": "Calory",
-            "protein": "Protein(g)",
-            "fat": "Fat(g)",
-            "carb": "Carbs(g)",
-            "date": "Date",
+
+        help_texts = {
+            'calory': 'kcal',
+            'protein': 'g',
+            'fat': 'g',
+            'carb': 'g',
         }
